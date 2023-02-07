c4e6a796a839223a6171caf74bff8c706be4993b
aaachris1820 <63766879+aaachris1820@users.noreply.github.com>
2023-02-07 11:13:15 +0600

    final version before submitting

--- module24-assignment-4/index.js
+++ module24-assignment-4/index.js
@@ -1,11 +1,13 @@
 /*
  Problem-1
-    The mindGame() function will take a number as input. Input will be multiplied by 3, added 10,divided by 2, subtracted by 5 and return the result as output.
+    The mindGame() function will take a positive number as input.
+     Input will be multiplied by 3, added 10,
+     divided by 2, subtracted by 5 and return the result as output.
  */
 
 function mindGame(inputMindGame) {
-    if (typeof inputMindGame !='number')
-        return 'Please enter a number';
+    if (typeof inputMindGame !='number' || inputMindGame<0)
+        return 'Please enter a positive number';
     else {
         let result=((inputMindGame*3+10)/2)-5;
         return result;
@@ -16,15 +18,17 @@ function mindGame(inputMindGame) {
 
 /* 
 Problem-2
-    The evenOdd function will take a string as input. It will count the character number of the input. Then return 'even' for even number of characters or return 'odd' for odd number of characters.
+    The evenOdd function will take a string as input. 
+    It will count the character number of the input. 
+    Then return 'even' for even number of characters 
+    or return 'odd' for odd number of characters.
 */
 
 function evenOdd(inputEvenOrOdd) {
 
     if (typeof inputEvenOrOdd !='string')
         return 'Please enter a string';
-    else if(inputEvenOrOdd==='')
-        return 'Empty string can not be even or odd'
+    
     else {
         if (inputEvenOrOdd.length%2==0) 
             return 'even';
@@ -36,7 +40,9 @@ function evenOdd(inputEvenOrOdd) {
 
 /* 
 Problem-3
-The function isLGSeven() will take a number as input. The input will be substracted by 7. If the substracted result is less than 7, it will be returned as ouput. If the substracted result is bigger than 7, it will be multiplied by 2 and returned as output.
+The function isLGSeven() will take a number as input.
+The input will be substracted by 7. If the substracted result is less than 7,
+it will be returned as ouput. Else input will be multiplied by 2 and returned as output.
 */
 
 function isLGSeven(inputLGSseven) {
@@ -51,11 +57,12 @@ function isLGSeven(inputLGSseven) {
         }
     }
 }
-console.log(isLGSeven(15));
+
 
 /* 
 Problem-4
-    The funciton findingBadData() will take an array of age numbers as input. It will count the number of negative numbers in the input and return the count.
+    The funciton findingBadData() will take an array of age numbers as input.
+    It will count the number of negative numbers in the input as bad data and return the total count.
 */
 
 function findingBadData(inputFindingBadData){
@@ -72,26 +79,27 @@ function findingBadData(inputFindingBadData){
         return countOfBadData;
     }
     else
-        return 'Please enter an array'
+        return 'Please enter an array';
 }
+
 
 /* 
 Problem-5
-    The function gemsToDiamond will take three numbers as input which are gems number of three friends. It will multiply the gems of first friend by 21, second friend by 32 and the third friend by 43 and add them. If the total is less than thousand multplied by two then return the total. If the total is equal or greater than thousand multiplied by two then substract it by 2000 and return it.
+    The function gemsToDiamond() will take three numbers as input which are gems power of three friends.
+    It will multiply the gems of first friend by 21, second friend by 32 and the third friend by 43 to convert them into diamonds and add them. If the total is less than thousand multplied by two then return the total. If the total is equal or greater than thousand multiplied by two then substract it by 2000 and return it.
 */
 
-function findingBadData(inputGemsFriend1,inputGemsFriend2,inputGemsFriend3) {
+function gemsToDiamond(inputGemsFriend1,inputGemsFriend2,inputGemsFriend3) {
 
     if (typeof inputGemsFriend1 != 'number' || typeof inputGemsFriend2 != 'number' || typeof inputGemsFriend3 != 'number') {
         return 'Please enter 3 numbers as input';
     }
     else {
-        let result=inputGemsFriend1*21+inputGemsFriend2*32+inputGemsFriend3*43;
+        let totalDiamond=inputGemsFriend1*21+inputGemsFriend2*32+inputGemsFriend3*43;
 
-        if ((result) >=2000)
-            return result-2000;
+        if (totalDiamond > 1000*2)
+            return totalDiamond-2000;
         else
-            return result;
+            return totalDiamond;
     }
 }
-console.log(findingBadData(200,0,0));
