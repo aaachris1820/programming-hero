5ca86a15b4fd46231ba0a52d5ff25f2faf1fb97b
aaachris1820 <63766879+aaachris1820@users.noreply.github.com>
2023-02-09 16:11:04 +0600

    adding css style via js

---
+++ module25-tour-of-dom/style.js
@@ -0,0 +1,7 @@
+let outsideListItem=document.getElementsByClassName('outside-list-item');
+outsideListItem[0].style.backgroundColor='yellow';
+outsideListItem[0].style.fontWeight='bold';
+let styleIdPracticeList=document.getElementById('id-practice-list');
+styleIdPracticeList.style.display='flex';
+styleIdPracticeList.style.columnGap='39px'
+styleIdPracticeList.style.justifyContent='space-around';
