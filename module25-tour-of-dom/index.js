06ab7014dfe8315d408e2f575e33f7063d3a76f3
aaachris1820 <63766879+aaachris1820@users.noreply.github.com>
2023-02-09 13:25:41 +0600

    tour of dom, accessing dom elements by using id,class,tagName,query,queryAll selectors, use of innerHTML,innerText. Concept of DOM, HTMLCollection,NodeList.

---
+++ module25-tour-of-dom/index.js
@@ -0,0 +1,30 @@
+let idPracticeList=document.getElementById('id-practice-list');
+console.log(typeof idPracticeList);
+console.log(idPracticeList);
+/* idPracticeList.innerHTML=`<ul>
+<li>list 1</li>
+</ul>`; */
+// idPracticeList.innerText='hello';
+
+let classPracticeList=document.getElementsByClassName('class-practice-list');
+console.log(typeof classPracticeList);
+console.log(classPracticeList);
+/* classPracticeList[0].innerHTML=`<ol>
+<li>list 1</li>
+</ol>` */
+// classPracticeList[0].innerText='sds';
+
+let queryPracticeList=document.querySelector('#id-practice-list');
+console.log(typeof queryPracticeList);
+console.log(queryPracticeList);
+// queryPracticeList.innerHTML same as getElementByID
+
+let queryAllPracticeList=document.querySelectorAll('li');
+console.log(typeof queryAllPracticeList);
+console.log(queryAllPracticeList);
+//Nodelist is kind of an array, can manipulate innerHTML by index like getElementsByClassNames
+
+let tagPracticeList=document.getElementsByTagName('li');
+console.log(typeof tagPracticeList);
+console.log(tagPracticeList);
+// innerHTML access process is same as getElementsByClassNames
