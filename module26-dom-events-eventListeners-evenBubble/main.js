6f92059f1290f35edbe1000a74ce210911c45548
aaachris1820 <63766879+aaachris1820@users.noreply.github.com>
2023-02-10 17:28:03 +0600

    adding a event listener on a submit button to add new paragraphs on a section

--- module26-dom-events-eventListeners-evenBubble/main.js
+++ module26-dom-events-eventListeners-evenBubble/main.js
@@ -1,9 +1,24 @@
 let paragraph1=document.getElementById('paragraph-1');
+let sectionParagraphs=document.getElementById('section-paragraphs');
+
 let inputText=document.getElementById('input-text');
-inputText.addEventListener('keyup',function(){
+inputText.addEventListener('keyup',function (){
     let inputValue= event.target.value;
+    
     console.log(inputValue);
     paragraph1.innerText=inputValue;
+    return paragraph1.innerText;
+});
+
+//button submit event to add the input as a new paragraph
+let buttonSubmit=document.getElementById('button-submit');
+console.log(buttonSubmit);
+buttonSubmit.addEventListener('click', function (){
+
+    let newParagraph=document.createElement('p');
+    newParagraph.innerText=inputText.value;
+    sectionParagraphs.appendChild(newParagraph);
+
 });
 
 let buttonReset=document.getElementById('button-reset');
