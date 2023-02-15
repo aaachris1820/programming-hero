5607da104e94b34813bbee6d0c61b5dadcf06721
aaachris1820 <63766879+aaachris1820@users.noreply.github.com>
2023-02-15 10:21:00 +0600

    added event listener to the remove images to reset count to 0 when clicked

--- module-29-shopping-cart-interactive-actions-pin-matcher-with-js/js/price.js
+++ module-29-shopping-cart-interactive-actions-pin-matcher-with-js/js/price.js
@@ -1,3 +1,21 @@
+// this function grabs count of iphone from id and calculates the total price of iphones and updates it on the html
+function updateIPhonePrice(){
+    const priceIPhone=1219;
+    let count=getValueOfInputById('count-iPhone');
+    let priceIphoneTotal=calculatePrice(priceIPhone,count);
+    setTextContentById(priceIphoneTotal,'price-iPhone');
+    return priceIphoneTotal;
+}
+
+// this function grabs count of case from id and calculates the total price of cases and updates it on the html
+function updateCasePrice(){
+    const priceCase=59;
+    let count=getValueOfInputById('count-case');
+    let priceCaseTotal=calculatePrice(priceCase,count);
+    setTextContentById(priceCaseTotal,'price-case');
+    return priceCaseTotal;
+}
+
 //event delegation to update phone price whenever a click happens in container with the buttons
 document.getElementById('container-cart-input-iPhone').addEventListener('click',function(){
     updateIPhonePrice();
@@ -8,18 +26,3 @@ document.getElementById('container-cart-input-case').addEventListener('click',fu
         updateCasePrice();
 })
 
-function updateIPhonePrice(){
-    const priceIPhone=1219;
-    let count=getValueOfInputById('count-iPhone');
-    let priceIphoneTotal=calculatePrice(priceIPhone,count);
-    setTextContentById(priceIphoneTotal,'price-iPhone');
-    return priceIphoneTotal;
-}
-
-function updateCasePrice(){
-    const priceCase=59;
-    let count=getValueOfInputById('count-case');
-    let priceCaseTotal=calculatePrice(priceCase,count);
-    setTextContentById(priceCaseTotal,'price-case');
-    return priceCaseTotal
-}
---
+++ module-29-shopping-cart-interactive-actions-pin-matcher-with-js/js/remove-count.js
@@ -0,0 +1,9 @@
+document.getElementById('remove-iPhone-count').addEventListener('click',function (){
+
+    setInputValueById(0,'count-iPhone');
+});
+
+document.getElementById('remove-case-count').addEventListener('click',function (){
+
+    setInputValueById(0,'count-case');
+});
