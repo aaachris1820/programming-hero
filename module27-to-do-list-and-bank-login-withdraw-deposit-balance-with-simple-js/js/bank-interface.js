9635c8dbd9e93ffd1ad7d263ba5420abaef5055a
aaachris1820 <63766879+aaachris1820@users.noreply.github.com>
2023-02-19 10:05:24 +0600

    fixed the issue of multiple events being triggered in delete button with use of stopImmediatePropagation function, also used event capturing to pass a click event to the new items container in the to do list

---
+++ module27-to-do-list-and-bank-login-withdraw-deposit-balance-with-simple-js/js/bank-interface.js
@@ -0,0 +1,68 @@
+//caputure both deposit and withdraw totals value and string
+let totalDeposit=document.getElementById('total-deposit');
+let totalDepositValue=parseFloat(document.getElementById('total-deposit').textContent);
+
+let totalWithdraw=document.getElementById('total-withdraw');
+let totalWithdrawValue=parseFloat(document.getElementById('total-withdraw').textContent);
+
+
+
+//capture balance total and value
+let totalBalance=document.getElementById('total-balance');
+let totalBalanceValue=parseFloat(document.getElementById('total-balance').textContent);
+
+console.log(totalBalance,totalDeposit,totalWithdraw);
+
+// deposit button event listener
+document.getElementById('button-deposit').addEventListener('click', function () {
+    //capture input deposit value
+    let inputDepositValue=parseFloat(document.getElementById('input-deposit').value);
+
+    if(inputDepositValue>=0) {
+        //update totalbalance and totaldeposit value
+        totalBalanceValue+=inputDepositValue;
+        totalDepositValue+=inputDepositValue
+        //update totalbalance and totaldeposit textcontent in html
+        totalBalance.textContent=totalBalanceValue.toFixed(2);
+        totalDeposit.textContent=totalDepositValue.toFixed(2);
+
+        console.log(inputDepositValue,totalDepositValue,totalBalanceValue);
+    }
+    
+});
+
+// withdraw button event listener
+
+document.getElementById('button-withdraw').addEventListener('click', function () {
+
+    //capture input withdraw value
+    let inputWithdrawValue=parseFloat(document.getElementById('input-withdraw').value);
+    console.log(inputWithdrawValue);
+    if (inputWithdrawValue>=0 &&inputWithdrawValue<=totalBalanceValue) {
+
+        //update totalbalance and totalwithdraw value
+        totalBalanceValue-=inputWithdrawValue;
+        totalWithdrawValue+=inputWithdrawValue
+
+        //update totalbalance and totaldeposit textcontent in html
+        totalBalance.textContent=totalBalanceValue.toFixed(2);
+        totalWithdraw.textContent=totalWithdrawValue.toFixed(2);
+
+        console.log(inputWithdrawValue,totalWithdrawValue,totalBalanceValue);
+    }
+    else if(inputWithdrawValue>totalBalanceValue)
+        {
+            let p=document.createElement('p');
+            p.innerText='Withdrawal amount can not be more than total balance';
+            p.style.color='red';
+            p.style.fontWeight='bold';
+            p.style.marginBottom='10px';
+            document.getElementById('error-message').appendChild(p);
+            setTimeout(function(){
+                p.remove();
+            },2500)
+        }
+    
+    
+
+});
