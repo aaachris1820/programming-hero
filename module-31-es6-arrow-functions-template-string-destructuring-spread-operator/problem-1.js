94988051aa3218adacc0b70a0ed9876962d9cc58
aaachris1820 <63766879+aaachris1820@users.noreply.github.com>
2023-02-20 18:05:50 +0600

    problem 1 done to multiply 3 numbers using arrow function

---
+++ module-31-es6-arrow-functions-template-string-destructuring-spread-operator/problem-1.js
@@ -0,0 +1,5 @@
+// an arrow function that will take 3 parameters, will multiply the parameters and will return the result.
+
+let multiply3=(num1,num2,num3)=>num1*num2*num3;
+
+console.log(multiply3(2,3,4));;
